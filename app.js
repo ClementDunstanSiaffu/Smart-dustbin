8ec53a7d8f8805fe2030ea6a6f45ccc6e544a0fc
unknown <clementsiaffu95@gmail.com>
2020-07-20 15:01:13 +0300

    third commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express();
 const PORT = process.env.PORT || 5000;
 const routes = require('./routes/index')
-
+const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
